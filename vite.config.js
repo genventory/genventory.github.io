1b3916eeb8349ab84c400fa05890ada77d8a35cb
genventory <ahmed.al7ikmani@gmail.com>
2024-04-18 19:33:42 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'build', // Ensure this directory matches what's expected in the deployment script
-  },
-  base: "/genventory/",
+  base: "/",
   plugins: [react()],
 })
